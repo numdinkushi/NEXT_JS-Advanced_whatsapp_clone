7dd12d7da41c7c8d641a3d013281850c9a648160
numdinkushi <numdinkushi@gmail.com>
2023-07-01 16:09:40 +0100

    wip: main chat screen

--- client/src/components/Chatlist/ChatListHeader.jsx
+++ client/src/components/Chatlist/ChatListHeader.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Avatar from "../common/Avatar";
+import { useStateProvider } from "@/context/StateContext";
 
 function ChatListHeader() {
-  return <div>ChatListHeader</div>;
+  const [{userInfo}, dispatch] = useStateProvider();
+console.log(userInfo);
+
+  return <div className="h-16 px-4 justify-between items-center ">
+   <div className="cursor-pointer">
+    <Avatar type="sm" image={userInfo?.profileImage} />
+   </div>
+   <div className="flex"></div>
+  </div>;
 }
 
 export default ChatListHeader;
