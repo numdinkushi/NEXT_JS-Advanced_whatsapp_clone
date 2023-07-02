1065b162218712cb15f2df0f697ae5059ce2fc46
numdinkushi <numdinkushi@gmail.com>
2023-07-02 16:29:49 +0100

    chat component added

--- client/src/components/Chat/MessageBar.jsx
+++ client/src/components/Chat/MessageBar.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { BsEmojiSmile } from "react-icons/bs";
+import { FaMicrophone } from "react-icons/fa";
+import { ImAttachment } from "react-icons/im";
+import { MdSend } from "react-icons/md";
 
 function MessageBar() {
-  return <div>MessageBar</div>;
+	return (
+		<div className="bg-panel-header-background h-20 px-4 flex items-center flex-center gap-6 relative">
+			<>
+				<div className="flex gap-6">
+					<BsEmojiSmile
+						className="text-panel-header-icon cursor-pointer text-xl"
+						title="Emoji"
+					/>
+					<ImAttachment
+						className="text-panel-header-icon cursor-pointer text-xl"
+						title="Attach File"
+					/>
+				</div>
+				<div className="w-full rounded-lg h-10 flex items-center">
+					<input
+						type="text"
+						className="bg-input-background text-sm focus:outline-none text-white h-10 rounded-lg px-5 py-4 w-full"
+					/>
+				</div>
+				<div className="flex w-10 items-center justify-center">
+					<button>
+						<MdSend
+							className="text-panel-header-icon cursor-pointer text-xl"
+							title="Send message"
+						/>
+						{/* <FaMicrophone
+          className="text-panel-header-icon cursor-pointer text-xl" title="Record message"
+          /> */}
+					</button>
+				</div>
+			</>
+		</div>
+	);
 }
 
 export default MessageBar;
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -8,6 +8,7 @@ import { CHECK_USER_ROUTE } from "@/utils/ApiRoutes";
 import { useRouter } from "next/router";
 import { useStateProvider } from "@/context/StateContext";
 import { reducerCases } from "@/context/constants";
+import Chat from "./Chat/Chat";
 
 function Main() {
 	const router = useRouter();
@@ -48,7 +49,8 @@ function Main() {
 		<>
 			<div className="grid grid-cols-main h-screen max-h-screen max-w-full overflow-hidden">
 				<ChatList />
-				<Empty />
+				<Chat />
+				{/* <Empty /> */}
 			</div>
 		</>
 	);
